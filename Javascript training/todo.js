7f60889a912465242f230d2f30fbb005efc4e66f
shigles <les.shigley@gmail.com>
2017-07-23 19:43:45 -0600

    changes to todo
    changes to todo

--- Javascript training/todo.js
+++ Javascript training/todo.js
@@ -1,10 +1,47 @@
-var todos =["feed hogs","do homework", "prep for invasion"];
+var todos = ["feed hogs"];
 
-var input = prompt ("What would you like to do?");
+var input = prompt("What would you like to do?");
 
-if(input === "list") {
-    console.log(todos);
-} else if(input === "new") {
+
+
+
+while (input !== "quit") {
+    //handles input
+    if (input === "list") {
+        listTodos();
+    //prompts for new todo
+    } else if (input === "new") {
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
+    }
+    //asks again for input
+    input = prompt("What would you like to do?");
+}
+
+console.log("OK! You quit the App!");
+
+
+function listTodos(){
+    console.log("**********");
+    todos.forEach(function (todo, i) {
+    console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function addTodo(){
     var newTodo = prompt("enter a new todo");
+    //adds todo to array
     todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo(){
+    //asks for the index to be deleted
+    var index = prompt("Enter the indexof the todo to delete");
+    //deletes specified index
+    //splice()
+    todos.splice(index,1);
+    console.log("Deleted Todo");
 }
